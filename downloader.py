b57f267b2cd46873fc2c90a8e33f526c6ff0160a
Gian <giansingh4710@gmail.com>
2021-12-27 22:37:04 -0500

    pass link from client side to server side

--- downloader.py
+++ downloader.py
@@ -1,19 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, url_for, render_template, send_file, request
+import requests
+from io import BytesIO
 
 app=Flask(__name__)
 
-# @app.route("/")
-# def home():
-#     return "HI prum py <h1>BOB</h1>"
+@app.route("/")
+def home():
+    return redirect(url_for('index'))
 
-# @app.route("/<name>")
-# def user(name):
-#     return f"HI <h1>{name}</h1>"
+@app.route("/index", methods=["POST","GET"])
+def index():
+    print(request.method)
+    if request.method=="POST":
+        theLink=request.form["link"]
+        theLink=theLink.replace("/","   ")
+        print("The Link: "+str(theLink))
+        return redirect(url_for("download_file", theLINK=theLink))
+    else:    
+        return render_template("index.html")
 
-@app.route("/<temp>")
-def index(temp):
-    return render_template("index.html",content=temp,lst=["vahe","guut","jio"])
-
+@app.route("/download/<theLINK>")
+def download_file(theLINK):
+    theLINK=theLINK.replace("   ","/")
+    return f"<h1>{theLINK}</h1>"
+    # r=requests.get(link)
+    # return send_file(BytesIO(r.content),attachment_filename="TEST.mp3", as_attachment=True)
 
 if __name__=="__main__":
-    app.run()
+    app.run(debug=True)
